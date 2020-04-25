4ff751c0e9d8e36a5193e7d6e9f8bd634945eb37
Manuel Lopez <manuelfx117@gmail.com>
2020-04-24 21:13:31 -0500

    asdafa

---
+++ build/precache-manifest.91592ae8403fd62f14f22dbf0131418c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e56ab40cfd1865f9fe892ec43d7e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b0a29bd8435006d959",
+    "url": "/static/css/2.dec9c0fa.chunk.css"
+  },
+  {
+    "revision": "a4b0a29bd8435006d959",
+    "url": "/static/js/2.3e2e4c21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3e2e4c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1befa4de10760e1a1864",
+    "url": "/static/js/main.7ec89a12.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "0a16744a57802b2990fb8ecac1bbc46a",
+    "url": "/static/media/comida.0a16744a.svg"
+  }
+]);
